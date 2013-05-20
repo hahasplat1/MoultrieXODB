5398a1c06a87fbeb6c6cddf0a9118d0d2e179981
dioptre <dioptre@gmail.com>
2013-05-20 14:11:52 +1000

    updated importer

--- src/main/XODB.Import/client/Properties/AssemblyInfo.cs
+++ src/main/XODB.Import/client/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ImportCSV")]
+[assembly: AssemblyTitle("XODB Import Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ImportCSV")]
+[assembly: AssemblyProduct("XODB Import Client")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
