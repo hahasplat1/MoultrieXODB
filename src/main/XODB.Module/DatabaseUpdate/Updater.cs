32d577500c813cdb67c1eb432e5fc4dcd8681afb
dioptre <dioptre@gmail.com>
2013-06-13 12:27:34 +1000

    updates for migration

--- src/main/XODB.Module/DatabaseUpdate/Updater.cs
+++ src/main/XODB.Module/DatabaseUpdate/Updater.cs
@@ -17,25 +17,6 @@ namespace XODB.Module.DatabaseUpdate
         public Updater(IObjectSpace objectSpace, Version currentDBVersion) : base(objectSpace, currentDBVersion) { }
         public override void UpdateDatabaseAfterUpdateSchema()
         {
-            object o = ExecuteScalarCommand("select [value] from X_PrivateData where UniqueID='XODBSchemaVersion'", false);
-            int xodbSchemaVersion = (o == null) ?  -1 : Convert.ToInt32(o);
-            if (xodbSchemaVersion == -1)
-                ExecuteNonQueryCommand(Properties.Resources.XODBSchema1Data, false);
-            if (xodbSchemaVersion < 2)
-                foreach (var s in Properties.Resources.XODBSchema2.Split(new string[] {"GO"}, StringSplitOptions.RemoveEmptyEntries)) ExecuteNonQueryCommand(s, true);
-            if (xodbSchemaVersion < 3)
-                foreach (var s in Properties.Resources.XODBSchema3.Split(new string[] { "GO" }, StringSplitOptions.RemoveEmptyEntries)) ExecuteNonQueryCommand(s, true); //This may have errors
-
-            try
-            {
-                if (xodbSchemaVersion != CurrentVersion && System.Windows.Forms.Application.ProductName.Contains("Win") && System.Windows.Forms.Application.ProductName.Contains("XODB"))
-                {
-                    System.Diagnostics.Process.Start(System.Windows.Forms.Application.ExecutablePath);
-                    System.Diagnostics.Process.GetCurrentProcess().Kill();
-                }
-            }
-            catch { }
-
             base.UpdateDatabaseAfterUpdateSchema();
         }
 
@@ -46,9 +27,13 @@ namespace XODB.Module.DatabaseUpdate
                 o = ExecuteScalarCommand("select [value] from X_PrivateData where UniqueID='XODBSchemaVersion'", false);
             int xodbSchemaVersion = (o == null) ? -1 : Convert.ToInt32(o);
             if (xodbSchemaVersion == -1)
-                ExecuteNonQueryCommand(Properties.Resources.XODBSchema1, false);                
+            {
+                ExecuteNonQueryCommand(Properties.Resources.XODBSchema1, false);
+                ExecuteNonQueryCommand(Properties.Resources.XODBSchema1Data, false);
+            }
             else if (xodbSchemaVersion != CurrentVersion && System.Windows.Forms.Application.ProductName.Contains("Win") && System.Windows.Forms.Application.ProductName.Contains("XODB"))
             {
+                //Do backup
                 try
                 {
                     var f = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "xodb.bak");
@@ -56,6 +41,20 @@ namespace XODB.Module.DatabaseUpdate
                 }
                 catch { }
             }
+            if (xodbSchemaVersion < 2)
+                foreach (var s in Properties.Resources.XODBSchema2.Split(new string[] { "GO" }, StringSplitOptions.RemoveEmptyEntries)) ExecuteNonQueryCommand(s, true);
+            if (xodbSchemaVersion < 3)
+                foreach (var s in Properties.Resources.XODBSchema3.Split(new string[] { "GO" }, StringSplitOptions.RemoveEmptyEntries)) ExecuteNonQueryCommand(s, true); //This may have errors
+            //Reboot
+            try
+            {
+                if (xodbSchemaVersion != CurrentVersion && System.Windows.Forms.Application.ProductName.Contains("Win") && System.Windows.Forms.Application.ProductName.Contains("XODB"))
+                {
+                    System.Diagnostics.Process.Start(System.Windows.Forms.Application.ExecutablePath);
+                    System.Diagnostics.Process.GetCurrentProcess().Kill();
+                }
+            }
+            catch { }
             base.UpdateDatabaseBeforeUpdateSchema();
         }
 
