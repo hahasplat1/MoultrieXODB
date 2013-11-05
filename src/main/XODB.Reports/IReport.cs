bffc5341161a54850ced512ab686bacb1a2a1d11
dioptre <dioptre@gmail.com>
2013-11-05 14:46:15 +1000

    added report viewer test

---
+++ src/main/XODB.Reports/IReport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DevExpress.XtraReports.UI;
+
+namespace WindowsApplication1
+{
+    public interface IReport
+    {
+        Dictionary<string,string> Filter { get; set; }
+
+    }
+}
